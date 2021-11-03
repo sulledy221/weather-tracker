8fdca04b23c2491ff41df94531362ceaff10e1e0
Sully's Mac <sully@Sullyss-MacBook-Pro.local>
2021-11-02 17:01:13 -0700

    start

---
+++ src/components/Weather.jsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from "react";
+
+export default function Weather() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  return <div></div>;
+}
